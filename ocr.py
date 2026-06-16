fdea64619eb587ae6191d2b32db43be046362d83
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-06-15 20:05:19 -0700

    Can save state of ANN

--- ocr.py
+++ ocr.py
@@ -1,8 +1,11 @@
 import numpy as np
 import math
+import json
 
 class NeuralNetwork:
     LEARNING_RATE = 0.5
+    _use_file = True
+    NN_FILE_PATH = "saved_ann.json"
 
     def _rand_initialize_weights(self, size_in, size_out):
         return [((x * 0.12) - 0.06) for x in np.random.rand(size_out, size_in)]
@@ -55,7 +58,28 @@ class NeuralNetwork:
         self.input_layer_bias += self.LEARNING_RATE * hidden_errors
 
     def save(self):
-        return
+        if not self._use_file:
+            return
+
+        json_neural_network = {
+            "theta1":[np_mat.tolist()[0] for np_mat in self.theta1],
+            "theta2":[np_mat.tolist()[0] for np_mat in self.theta2],
+            "b1":self.input_layer_bias[0].tolist()[0],
+            "b2":self.hidden_layer_bias[0].tolist()[0]
+        };
+        with open(self.NN_FILE_PATH,'w') as nnFile:
+            json.dump(json_neural_network, nnFile)
+
+    def _load(self):
+        if not self._use_file:
+            return
+
+        with open(self.NN_FILE_PATH) as nnFile:
+            nn = json.load(nnFile)
+        self.theta1 = [np.array(li) for li in nn['theta1']]
+        self.theta2 = [np.array(li) for li in nn['theta2']]
+        self.input_layer_bias = [np.array(nn['b1'][0])]
+        self.hidden_layer_bias = [np.array(nn['b2'][0])]
     
     def predict(self, test):
         predictions = self.forward_propogate(test)['predictions']
