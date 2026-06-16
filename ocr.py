7f9964f7ff34e7f4001cc51492ca7917fa24326c
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2026-06-15 17:08:17 -0700

    forward_propogate function no longer duplicates code in predict; added train function

--- ocr.py
+++ ocr.py
@@ -20,14 +20,17 @@ class NeuralNetwork:
         new_matrix = sigmoid_to_matrix(matrix)
         return new_matrix
 
-    def forward_propogate(self):
-        y1 = np.dot(np.asmatrix(self.theta1), np.map(data['y0']).T)
-        sum1 = y1 + np.asmatrix(self.input_layer_bias)
-        y1 = self.sigmoid(sum1)
+    def forward_propogate(self, pixels):
+        y1 = np.dot(np.asmatrix(self.theta1), np.asmatrix(pixels).T)
+        y1 = y1 + np.asmatrix(self.input_layer_bias)
+        y1 = self.sigmoid(y1)
     
         y2 = np.dot(np.array(self.theta2), y1)
-        y2 = np.add(y2, self.hidden_layer_bias)
+        y2 = y2 + np.asmatrix(self.hidden_layer_bias)
         y2 = self.sigmoid(y2)
+
+        results = y2.T.tolist()[0]
+        return results
     
     def back_propogate(self):
         actual_vals = [0] * 10
@@ -41,17 +44,22 @@ class NeuralNetwork:
         self.input_layer_bias += self.LEARNING_RATE * hidden_errors
     
     def predict(self, test):
-        y1 = np.dot(np.asmatrix(self.theta1), np.asmatrix(test).T)
-        y1 = y1 + np.asmatrix(self.input_layer_bias)
-        y1 = self.sigmoid(y1)
+        results = self.forward_propogate(test)
+
     
-        y2 = np.dot(np.array(self.theta2), y1)
-        y2 = y2 + np.asmatrix(self.hidden_layer_bias)
-        y2 = self.sigmoid(y2)
-    
-        results = y2.T.tolist()[0]
+        # results = y2.T.tolist()[0]
         highest_confidence = max(results)
         confidence_percent = int(highest_confidence * 100)
         prediction = results.index(highest_confidence)
         print("Predicting digit is {0} with {1}% confidence".format(prediction, confidence_percent))
         return results.index(max(results))
+
+    def train_on_example(self, pixels, digit):
+        self.predict(pixels)
+        print("Actual digit is {0}".format(digit))
+        return
+
+    def train(self, training_data):
+        for example in training_data:
+            self.train_on_example(example['y0'], example['label'])
+        return
